7865f5ef5630983a15748ef9e06644a2d469ff19
SEEDART007 <dashranger60@gmail.com>
2025-03-28 00:28:26 +0530

    Handled all unwanted routes

--- app.js
+++ app.js
@@ -16,6 +16,12 @@ app.use(express.json())
 
 app.use('/api/tours',tourRouter)
 app.use('/api/users',userRouter)
+app.all('*',(req,res,next)=>{
+    res.status(404).json({
+        status:'fail',
+        message:`Can't find ${req.originalUrl}`
+    })
+})
 
 
 
