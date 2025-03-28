e18e87655df65f36dcfffb07ac80612a5e14cf94
SEEDART007 <dashranger60@gmail.com>
2025-03-28 15:31:41 +0530

    Improved GlobalErrorHandling

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const app = express()
+const globalErrorHandler = require('./controllers/errorController')
 const tourRouter = require('./routes/tourRoute')
 const userRouter= require('./routes/userRoute')
+const AppError = require('./utils/appError')
 
 
 //middleware
@@ -17,21 +19,10 @@ app.use(express.json())
 app.use('/api/tours',tourRouter)
 app.use('/api/users',userRouter)
 app.all('*',(req,res,next)=>{
-    const err = new Error(`Can't find ${req.originalUrl}`)
-    err.status='fail';
-    err.statusCode=404;
-    next(err);
+    next(new AppError(`Can't find ${req.originalUrl}`,404));
 })
 
-app.use((err,req,res,next)=>{
-    err.statusCode = err.statusCode || 500;
-    err.status = err.status || 'Internal Server Error';
-    res.status(err.statusCode).json({
-        status:err.status,
-        message:err.message
-    })
-  
-})
+app.use(globalErrorHandler)
 
 
 
