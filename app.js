eadd234aacd8ea2532fdd067b69b1b390b5b7452
SEEDART007 <dashranger60@gmail.com>
2025-03-28 15:16:59 +0530

    Added global error handler

--- app.js
+++ app.js
@@ -17,10 +17,20 @@ app.use(express.json())
 app.use('/api/tours',tourRouter)
 app.use('/api/users',userRouter)
 app.all('*',(req,res,next)=>{
-    res.status(404).json({
-        status:'fail',
-        message:`Can't find ${req.originalUrl}`
+    const err = new Error(`Can't find ${req.originalUrl}`)
+    err.status='fail';
+    err.statusCode=404;
+    next(err);
+})
+
+app.use((err,req,res,next)=>{
+    err.statusCode = err.statusCode || 500;
+    err.status = err.status || 'Internal Server Error';
+    res.status(err.statusCode).json({
+        status:err.status,
+        message:err.message
     })
+  
 })
 
 
