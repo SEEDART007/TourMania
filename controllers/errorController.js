19d3de1006d347325d103f3bae079154fe08598e
SEEDART007 <dashranger60@gmail.com>
2025-03-29 19:26:25 +0530

    Handled production & development errors

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,9 +1,37 @@
+//for development error
+const sendErrorDev=(err,res)=>{
+    res.status(err.statusCode).json({
+        status:err.status,
+        err:err,
+        message:err.message,
+        stack:err.stack
+    })
+}
+//for production error
+const sendErrorProduction=(err,res)=>{
+    //operational error
+    if(err.isOperational){
+        res.status(err.statusCode).json({
+            status:err.status,
+            message:err.message,
+        })  
+        //some unknown or programming error
+    }else{
+        console.error('ERROR❤️',err);
+        res.status(500).json({
+            status:'eror',
+            message:'Something went wrong!!!'
+        })
+    }
+}
+
 module.exports = (err,req,res,next)=>{
     err.statusCode = err.statusCode || 500;
     err.status = err.status || 'Internal Server Error';
-    res.status(err.statusCode).json({
-        status:err.status,
-        message:err.message
-    })
+      if(process.env.NODE_ENV ==='development'){
+       sendErrorDev(err,res);
+      }else if(process.env.NODE_ENV ==='production'){
+          sendErrorProduction(err,res)
+      } 
   
 }
