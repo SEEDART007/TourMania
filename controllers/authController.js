1888c251fdec181ad39b02ff78049b4009a98456
SEEDART007 <dashranger60@gmail.com>
2025-04-25 10:12:04 +0530

    Added signup using JWT

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,13 +1,24 @@
 const User = require('../models/userModel')
 const catchAsync = require("../utils/catchAsync");
+const jwt = require('jsonwebtoken')
 
 
 exports.signup=catchAsync(async(req,res,next)=>{
-    const newUser = await User.create(req.body);
+    const newUser = await User.create({
+        name:req.body.name,
+        email:req.body.email,
+        password:req.body.password,
+        confirmPassword:req.body.confirmPassword,
+    });
+
+    const token = jwt.sign({id:newUser._id},'secret',{
+        expiresIn:'90d'
+    })
 
 
     res.status(201).json({
        status:'success',
+       token,
         data:{
            user: newUser
         }
