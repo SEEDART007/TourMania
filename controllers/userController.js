56236769dd55630319bc9a164f59beffbede4352
SEEDART007 <dashranger60@gmail.com>
2025-04-30 10:00:52 +0530

    Update user data route added

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,31 @@
+const User = require("../models/userModel")
+const AppError = require("../utils/appError")
+const catchAsync = require("../utils/catchAsync")
+const filterObj = (obj,...allowedFields)=>{
+    const newObj={}
+    Object.keys(obj).forEach(el=>{
+        if(allowedFields.includes(el)){
+            newObj[el]=obj[el]
+        }
+    })
+    return newObj;
+}
 exports.getUser=(req,res)=>{
     res.status(200).json({
         message:'you have reached to user route'
     })
 }
+
+exports.updateMe = catchAsync(async(req,res,next)=>{
+    if(req.body.password || req.body.confirmPassword){
+        return next(new AppError('This route is not for updating password, use updateMyPassword route',400))
+    }
+    const updateBody = filterObj(req.body,'name','email')
+    const updatedUser = await User.findByIdAndUpdate(req.user.id,updateBody,{
+        new:true,runValidators:true
+    })
+    res.status(200).json({
+        status:'success',
+        updatedUser
+    })
+})
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const {getUser} = require('./../controllers/userController')
 const authController = require('./../controllers/authController')
+const userController = require('./../controllers/userController')
 const router = express.Router();
 
 router.post('/signup',authController.signup)
@@ -9,6 +10,7 @@ router.post('/login',authController.login)
 router.post('/forgotpassword',authController.forgotPassword)
 router.patch('/resetpassword/:token',authController.resetPassword)
 router.patch('/updateMyPassword',authController.protect,authController.updatePassword)
+router.patch('/updateMe',authController.protect,userController.updateMe)
 
 router.route('/').get(getUser)
 
